910a0cb312af81de13e6e86fb061f4f0bb177f09
david <depch47@gmail.com>
2021-09-09 12:17:23 -0500

    temperature feature is ready

--- src/temperatureConverter.js
+++ src/temperatureConverter.js
@@ -1,1 +1,12 @@
 // implement two functions to change from Celsius to Fahrenheit and vice versa.
+function cToF(celsius) {
+  const cTemp = celsius;
+  const cToFahr = cTemp * 9 / 5 + 32;
+  return cToFahr;
+}
+
+function fToC(fahrenheit) {
+  const fTemp = fahrenheit;
+  const fToCel = (fTemp - 32) * 5 / 9;
+  return fToCel;
+}
