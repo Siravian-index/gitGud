14bd237af0f3d472a09f83cb06552257fef69842
david <depch47@gmail.com>
2021-09-09 12:05:19 -0500

    new feature in progress

--- src/filterOddNum.js
+++ src/filterOddNum.js
@@ -1,9 +1,6 @@
 // a simple function
-
 const filterOddNumbers = (arr) => {
   return arr.filter((ele) => ele % 2 === 0);
 }
-
-const arrayTest = [1,2,3,4,5,6,7,8]
-
-console.log(filterOddNumbers(arrayTest))
+const arrayTest = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(filterOddNumbers(arrayTest));
---
+++ src/temperatureConverter.js
@@ -0,0 +1,1 @@
+// implement two functions to change from Celsius to Fahrenheit and vice versa.
